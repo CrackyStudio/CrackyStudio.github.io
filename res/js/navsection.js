4fbd2adc2be1fbbe40a307e6dd7a3df798f38872
Antoine Nivoy <antoine.nivoy@gmail.com>
2018-08-08 15:41:51 +0200

    commit

--- res/js/navsection.js
+++ res/js/navsection.js
@@ -2,36 +2,30 @@ function Dev() {
     var x = document.getElementById("devitems");
     var x2 = document.getElementById("aboutmeitems");
     var x3 = document.getElementById("musicitems");
-    if (x.style.display === "none") {
+    if (x.style.display = "none") {
         x.style.display = "block";
         x2.style.display = "none";
         x3.style.display = "none"; 
-    } else {
-        x.style.display = "none";
     }
 }
 function AboutMe() {
     var x = document.getElementById("aboutmeitems");
     var x2 = document.getElementById("devitems");
     var x3 = document.getElementById("musicitems");
-    if (x.style.display === "none") {
+    if (x.style.display = "none") {
         x.style.display = "block";
         x2.style.display = "none";  
         x3.style.display = "none"; 
-    } else {
-        x.style.display = "none";
     }
 }
 function Music() {
     var x = document.getElementById("musicitems");
     var x2 = document.getElementById("aboutmeitems");
     var x3 = document.getElementById("devitems");
-    if (x.style.display === "none") {
+    if (x.style.display = "none") {
         x.style.display = "block";
         x2.style.display = "none"; 
         x3.style.display = "none"; 
-    } else {
-        x.style.display = "none";
     }
 }
 function NoItem() {
